6218bf578e3faca13bebf11c48221ba83f814997
mikolalysenko <mikolalysenko@gmail.com>
2017-03-20 12:44:53 -1000

    add readme and update publish script

---
+++ README.md
@@ -0,0 +1,3 @@
+# 0hz.life
+
+This is a janky little script for maintaining my new blog at 0hz.life
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,3 +1,31 @@
+var fs = require('fs')
+var path = require('path')
+var glob = require('glob')
 var NeoCities = require('neocities')
 var credentials = require('../credentials.json')
 var api = new NeoCities(credentials.username, credentials.password)
+
+var BUILD_DIR = path.join(__dirname, '../build')
+
+glob(BUILD_DIR + '/**', function (err, files) {
+  if (err) {
+    return console.error(err)
+  }
+
+  var output = []
+  files.forEach(function (file) {
+    var rel = path.relative(BUILD_DIR, file)
+    if (!rel) {
+      return
+    }
+    var stats = fs.lstatSync(file)
+    if (!stats.isDirectory()) {
+      output.push({
+        name: rel, path: file
+      })
+    }
+  })
+  api.upload(output, function (resp) {
+    console.log(resp)
+  })
+})
